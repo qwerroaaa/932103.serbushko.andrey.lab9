fbdf8e724037f07bfc1e538ac02fca6b8e60bace
qwerroaaa <serbushko.qwe@gmail.com>
2023-12-02 12:04:43 +0700

    added change operation

--- script.js
+++ script.js
@@ -1,126 +1,66 @@
-class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-      this.previousOperandTextElement = previousOperandTextElement
-      this.currentOperandTextElement = currentOperandTextElement
-      this.clear()
+const operation = '+-/*';
+const digits = '1234567890';
+var previousOperandTextElement = document.querySelector('[data-previous-operand]');
+var currentOperandTextElement = document.querySelector('[data-current-operand]');
+var numberButtons = document.querySelectorAll('[data-number]');
+
+currentOperandTextElement.textContent = '0';
+
+function isValid(str) {
+  return /^[-+*/0-9.]+$/.test(str);
+}
+
+function clearDisplay() {
+  currentOperandTextElement.textContent = '0';
+  previousOperandTextElement.textContent = '';
+}
+
+function addNumber(number) {
+    if (currentOperandTextElement.textContent == '0' || !(isValid(currentOperandTextElement.textContent))) {
+      currentOperandTextElement.textContent = ''
     }
-  
-    clear() {
-      this.currentOperand = ''
-      this.previousOperand = ''
-      this.operation = undefined
+    currentOperandTextElement.textContent += number;
+}
+
+function applyOperation(operation)
+ {
+    if(currentOperandTextElement.textContent == '')
+    {
+        previousOperandTextElement.textContent = previousOperandTextElement.textContent.slice(0, -1) + operation;
+        return;
     }
-  
-    delete() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    if(!isValid(currentOperandTextElement.textContent))
+    {
+        clearDisplay();
+        return;
     }
-  
-    appendNumber(number) {
-      if (number === '.' && this.currentOperand.includes('.')) return
-      this.currentOperand = this.currentOperand.toString() + number.toString()
+    if(currentOperandTextElement.textContent[currentOperandTextElement.textContent.length - 1] == '.') currentOperandTextElement.textContent = currentOperandTextElement.textContent.slice(0, -1);
+
+    previousOperandTextElement.textContent += currentOperandTextElement.textContent + ' ' + operation;
+    currentOperandTextElement.textContent = '';
+
+}
+
+function calculate()
+{
+    currentOperandTextElement.textContent = eval(previousOperandTextElement.textContent + currentOperandTextElement.textContent);
+    previousOperandTextElement.textContent = '';
+}
+
+function addDot()
+{
+    if(isValid(currentOperandTextElement.textContent) && currentOperandTextElement.textContent[currentOperandTextElement.textContent.length - 1] != '.')
+        currentOperandTextElement.textContent += '.';
+}
+
+function back()
+{
+    if(!isValid(currentOperandTextElement.textContent) )
+    {
+        clearDisplay();
+        return;
     }
-  
-    chooseOperation(operation) {
-      if (this.currentOperand === '') return
-      if (this.previousOperand !== '') {
-        this.compute()
-      }
-      this.operation = operation
-      this.previousOperand = this.currentOperand
-      this.currentOperand = ''
-    }
-  
-    compute() {
-      let computation
-      const prev = parseFloat(this.previousOperand)
-      const current = parseFloat(this.currentOperand)
-      if (isNaN(prev) || isNaN(current)) return
-      switch (this.operation) {
-        case '+':
-          computation = prev + current
-          break
-        case '-':
-          computation = prev - current
-          break
-        case '*':
-          computation = prev * current
-          break
-        case '/':
-            computation = prev / current
-            break
-        default:
-          return
-      }
-      this.currentOperand = computation
-      this.operation = undefined
-      this.previousOperand = ''
-    }
-  
-    getDisplayNumber(number) {
-      const stringNumber = number.toString()
-      const integerDigits = parseFloat(stringNumber.split('.')[0])
-      const decimalDigits = stringNumber.split('.')[1]
-      let integerDisplay
-      if (isNaN(integerDigits)) {
-        integerDisplay = ''
-      } else {
-        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-      }
-      if (decimalDigits != null) {
-        return `${integerDisplay}.${decimalDigits}`
-      } else {
-        return integerDisplay
-      }
-    }
-  
-    updateDisplay() {
-      this.currentOperandTextElement.innerText =
-        this.getDisplayNumber(this.currentOperand)
-      if (this.operation != null) {
-        this.previousOperandTextElement.innerText =
-          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-      } else {
-        this.previousOperandTextElement.innerText = ''
-      }
-    }
-  }
-  
-  
-  const numberButtons = document.querySelectorAll('[data-number]')
-  const operationButtons = document.querySelectorAll('[data-operation]')
-  const equalsButton = document.querySelector('[data-equals]')
-  const deleteButton = document.querySelector('[data-delete]')
-  const allClearButton = document.querySelector('[data-all-clear]')
-  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-  const currentOperandTextElement = document.querySelector('[data-current-operand]')
-  
-  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-  
-  numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      calculator.appendNumber(button.innerText)
-      calculator.updateDisplay()
-    })
-  })
-  
-  operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      calculator.chooseOperation(button.innerText)
-      calculator.updateDisplay()
-    })
-  })
-  
-  equalsButton.addEventListener('click', button => {
-    calculator.compute()
-    calculator.updateDisplay()
-  })
-  
-  allClearButton.addEventListener('click', button => {
-    calculator.clear()
-    calculator.updateDisplay()
-  })
-  
-  deleteButton.addEventListener('click', button => {
-    calculator.delete()
-    calculator.updateDisplay()
-  })
+    if(currentOperandTextElement.textContent.length == 1) currentOperandTextElement.textContent = '0';
+    else currentOperandTextElement.textContent = currentOperandTextElement.textContent.slice(0, -1);
+}
+
